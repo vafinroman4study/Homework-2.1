340f552a70f908219f6edb03a775316614fa3235
roman <romanvafin92@gmail.com>
2023-03-11 16:21:35 +0300

    Первое решение №2

--- src/Gryffindor.java
+++ src/Gryffindor.java
@@ -1,7 +1,7 @@
 public class Gryffindor extends Hogwarts {
-    private int courage;    // храбрость
-    private int honor;      // честь
-    private int chivalry;   // благородство
+    int courage;    // храбрость
+    int honor;      // честь
+    int chivalry;   // благородство
 
     public Gryffindor(String firstName, String lastName, int sorcery, int transgression, int courage, int honor, int chivalry) {
         super(firstName, lastName, sorcery, transgression);
